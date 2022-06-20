249ace42d607fe023b9cb02074925b4c3091770a
achmadbias24 <achmadbias24@gmail.com>
2022-06-20 10:12:08 +0700

    update jadwal

--- application/views/admin/jadwal/index.php
+++ application/views/admin/jadwal/index.php
@@ -2,50 +2,67 @@
             Content body start
         ***********************************-->
 		<div class="content-body">
-		  <!-- row -->
-		  <div class="container-fluid">
-		    <div class="form-head d-flex mb-sm-4 mb-3">
-		      <div class="mr-auto">
-		        <h2 class="text-black font-w600">Jadwal</h2>
-		        <p class="mb-0">Dashboard Admin RSUD Kabupaten Jombang</p>
-		      </div>
-		    </div>
-		    <div class="row">
-		      <div class="col-12">
-		        <form action="">
-		          <div class="mb-5">
-		            Tampilkan jadwal dokter pada tanggal : <input type="date" class="form-control" id="tgl_jadwal" name="tgl_jadwal">
-		          </div>
-		        </form>
-		        <div class="table-responsive card-table">
-		          <table id="example5" class="display dataTablesCard table-responsive-xl">
-		            <thead class="text-center">
-		              <tr>
-		                <th>ID Jadwal</th>
-		                <th>Nama Dokter</th>
-		                <th>Jam Awal</th>
-		                <th>Jam Akhir</th>
-		                <th>Aksi</th>
-		              </tr>
-		            </thead>
-		            <tbody class="text-center">
-		              <tr>
-		                <td><span class="text-nowrap">PJ-513</span></td>
-		                <td>Dr. Samantha</td>
-		                <td>09.00 WIB</td>
-		                <td>10.00 WIB</td>
-		                <td>
-		                  <button type="button" class="btn btn-primary">
-		                    Detail
-		                  </button>
-		                </td>
-		              </tr>
-		            </tbody>
-		          </table>
-		        </div>
-		      </div>
-		    </div>
-		  </div>
+			<!-- row -->
+			<div class="container-fluid">
+				<div class="form-head d-flex mb-sm-4 mb-3">
+					<div class="mr-auto">
+						<h2 class="text-black font-w600">Jadwal</h2>
+						<p class="mb-0">Dashboard Admin RSUD Kabupaten Jombang</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-12">
+						<div class="table-responsive card-table">
+							<table id="example5" class="display dataTablesCard table-responsive-xl">
+								<thead class="text-center">
+									<tr>
+										<th>Nama Dokter</th>
+										<th>Senin</th>
+										<th>Selasa</th>
+										<th>Rabu</th>
+										<th>Kamis</th>
+										<th>Jumat</th>
+										<th>Aksi</th>
+									</tr>
+								</thead>
+								<tbody class="text-center">
+									<tr>
+										<td>
+											<img src="" alt="FOTO DOKTER"><br>
+											<span>
+												Dr. Samantha
+												Umum
+											</span>
+										</td>
+										<td>
+											<h5>
+												<div class="badge badge-success">16.00-17.00 WIB</div>
+											</h5>
+										</td>
+										<td></td>
+										<td>
+											<h5>
+												<div class="badge badge-success">16.00-17.00 WIB</div>
+											</h5>
+										</td>
+										<td></td>
+										<td>
+											<h5>
+												<div class="badge badge-success">16.00-17.00 WIB</div>
+											</h5>
+										</td>
+										<td>
+											<button type="button" class="btn btn-primary">
+												Detail
+											</button>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 		<!--**********************************
             Content body end
