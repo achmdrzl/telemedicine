75850c2a27185823010aad0c552b4182beb5a7f8
Wildani Sulthon Mahmudi <54610995+Kaneisan@users.noreply.github.com>
2022-06-22 12:12:39 +0700

    header footer login

---
+++ application/views/pasien/template/header.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <meta name="description" content="Medcity - Medical Healthcare HTML5 Template">
+  <link href="assets/images/favicon/favicon.png" rel="icon">
+  <title>Medcity - Medical Healthcare HTML5 Template</title>
+
+  <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600;700&family=Roboto:wght@400;700&display=swap">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css">
+  <link rel="stylesheet" href="assets/css/libraries.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+  <div class="wrapper">
+    <div class="preloader">
+      <div class="loading"><span></span><span></span><span></span><span></span></div>
+    </div><!-- /.preloader -->
+
+    <!-- =========================
+        Header
+    =========================== -->
+    <header class="header header-layout2">
+      <div class="header-topbar">
+        <div class="container-fluid">
+          <div class="row align-items-center">
+            <div class="col-12">
+              <div class="d-flex align-items-center justify-content-between">
+                <ul class="contact__list d-flex flex-wrap align-items-center list-unstyled mb-0">
+                  <li>
+                    <i class="icon-phone"></i><a href="tel:+5565454117">Emergency Line: (002) 01061245741</a>
+                  </li>
+                  <li>
+                    <i class="icon-location"></i><a href="#">Location: Brooklyn, New York</a>
+                  </li>
+                  <li>
+                    <i class="icon-clock"></i><a href="contact-us.html">Mon - Fri: 8:00 am - 7:00 pm</a>
+                  </li>
+                </ul><!-- /.contact__list -->
+                <div class="d-flex">
+                  <ul class="social-icons list-unstyled mb-0 mr-30">
+                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                  </ul><!-- /.social-icons -->
+                  <div class="dropdown lang-dropdown">
+                    <button class="dropdown-toggle lang-dropdown-toggle" id="langDropdown" data-toggle="dropdown">
+                      <img src="assets/images/flags/en.png" alt="en"> <span>English</span>
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="langDropdown">
+                      <a class="dropdown-item" href="#">
+                        <img src="assets/images/flags/fr.png" alt="en"> <span>France</span>
+                      </a>
+                      <a class="dropdown-item" href="#">
+                        <img src="assets/images/flags/gr.png" alt="en"> <span>Germany</span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div><!-- /.col-12 -->
+          </div><!-- /.row -->
+        </div><!-- /.container -->
+      </div><!-- /.header-top -->
+      <nav class="navbar navbar-expand-lg sticky-navbar">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="index.html">
+            <img src="assets/images/logo/logo-light.png" class="logo-light" alt="logo">
+            <img src="assets/images/logo/logo-dark.png" class="logo-dark" alt="logo">
+          </a>
+          <button class="navbar-toggler" type="button">
+            <span class="menu-lines"><span></span></span>
+          </button>
+          <div class="collapse navbar-collapse" id="mainNavigation">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav__item has-dropdown">
+                <a href="#" data-toggle="dropdown" class="dropdown-toggle nav__item-link active">Home</a>
+              </li><!-- /.nav-item -->
+              <li class="nav__item has-dropdown">
+                <a href="#" data-toggle="dropdown" class="dropdown-toggle nav__item-link">About Us</a>
+              </li><!-- /.nav-item -->
+              <li class="nav__item has-dropdown">
+                <a href="#" data-toggle="dropdown" class="dropdown-toggle nav__item-link">Doctors</a>
+              </li><!-- /.nav-item -->
+              <li class="nav__item has-dropdown">
+                <a href="#" data-toggle="dropdown" class="dropdown-toggle nav__item-link">Layanan</a>
+                <ul class="dropdown-menu">
+                  <li class="nav__item">
+                    <a href="blog.html" class="nav__item-link">Konsultasi Online</a>
+                  </li><!-- /.nav-item -->
+                </ul><!-- /.dropdown-menu -->
+              </li><!-- /.nav-item -->
+              <li class="nav__item has-dropdown">
+                <a href="#" data-toggle="dropdown" class="dropdown-toggle nav__item-link">Informasi</a>
+                <ul class="dropdown-menu">
+                  <li class="nav__item">
+                    <a href="shop.html" class="nav__item-link">Our Products</a>
+                  </li><!-- /.nav-item -->
+                  <li class="nav__item">
+                    <a href="shop-single-product.html" class="nav__item-link">Products Single</a>
+                  </li><!-- /.nav-item -->
+                  <li class="nav__item">
+                    <a href="cart.html" class="nav__item-link">Cart</a>
+                  </li><!-- /.nav-item -->
+                </ul><!-- /.dropdown-menu -->
+              </li><!-- /.nav-item -->
+              <li class="nav__item">
+                <a href="contact-us.html" class="nav__item-link">Contacts</a>
+              </li><!-- /.nav-item -->
+            </ul><!-- /.navbar-nav -->
+            <button class="close-mobile-menu d-block d-lg-none"><i class="fas fa-times"></i></button>
+          </div><!-- /.navbar-collapse -->
+          <button class="action__btn-search ml-30"><i class="fa fa-search"></i></button>
+          <div class="d-none d-xl-flex align-items-center position-relative ml-0">
+            <ul id="miniPopup-departments" class="miniPopup miniPopup-departments dropdown-menu">
+              <li class="nav__item">
+                <a href="department-single.html" class="nav__item-link">Neurology Clinic</a>
+              </li><!-- /.nav-item -->
+              <li class="nav__item">
+                <a href="department-single.html" class="nav__item-link">Cardiology Clinic</a>
+              </li><!-- /.nav-item -->
+              <li class="nav__item">
+                <a href="department-single.html" class="nav__item-link">Pathology Clinic</a>
+              </li><!-- /.nav-item -->
+              <li class="nav__item">
+                <a href="department-single.html" class="nav__item-link">Laboratory Clinic</a>
+              </li><!-- /.nav-item -->
+              <li class="nav__item">
+                <a href="department-single.html" class="nav__item-link">Pediatric Clinic</a>
+              </li><!-- /.nav-item -->
+              <li class="nav__item">
+                <a href="department-single.html" class="nav__item-link">Cardiac Clinic</a>
+              </li><!-- /.nav-item -->
+            </ul> <!-- /.miniPopup-departments -->
+            <a href="appointment.html" class="btn btn__primary btn__rounded ml-30">
+              <i class="icon-calendar"></i>
+              <span>Login</span>
+            </a>
+          </div>
+        </div><!-- /.container -->
+      </nav><!-- /.navabr -->
+    </header><!-- /.Header -->
