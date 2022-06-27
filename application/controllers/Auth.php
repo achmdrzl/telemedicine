06f65c55df95c14533c6b9a03fc50344ecc31fe1
achmdrzl <achmd.rzl13@gmail.com>
2022-06-27 19:15:30 +0700

    register update

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -1,22 +1,64 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Welcome extends CI_Controller
+class Auth extends CI_Controller
 {
-    public function proses()
+    public function __construct()
     {
-        $this->form_validation->set_rules('username', 'username', 'trim|required|min_length[1]|max_length[255]|is_unique[user.username]');
-        $this->form_validation->set_rules('password', 'password', 'trim|required|min_length[1]|max_length[255]');
-        $this->form_validation->set_rules('confirmpassword', 'confirmpassword', 'trim|required|min_length[1]|max_length[255]');
+        parent::__construct();
+        $this->load->model('auth_pasien');
+
+    }
+
+    public function register()
+    {
+        $this->form_validation->set_rules('name', 'name', 'trim|required|min_length[1]|max_length[50]|is_unique[pasien.NAMA_PASIEN]');
+        $this->form_validation->set_rules('email', 'email', 'trim|required|min_length[1]|max_length[50]');
+        $this->form_validation->set_rules('password', 'password', 'trim|required|min_length[1]|max_length[25]');
+        $this->form_validation->set_rules('nohp', 'nohp', 'trim|required|min_length[1]|max_length[25]');
         if ($this->form_validation->run() == true) {
-            $username = $this->input->post('username');
+            $name = $this->input->post('name');
+            $username = $this->input->post('email');
             $password = $this->input->post('password');
-            $this->auth_pasien->register($username, $password);
+            $nohp = $this->input->post('nohp');
+            $this->auth_pasien->register($name, $username, $password, $nohp);
             $this->session->set_flashdata('success_register', 'Proses Pendaftaran User Berhasil');
-            redirect('welcome/login');
+            redirect('welcome/verif');
         } else {
             $this->session->set_flashdata('error', validation_errors());
             redirect('welcome/register');
         }
+
     }
+
+    public function verif()
+    {
+        $this->form_validation->set_rules('otp', 'otp', 'trim|required|min_length[4]|max_length[4]');
+        if ($this->form_validation->run() == true) {
+            $otp = $this->input->post('otp');
+            $this->auth_pasien->verif($otp);
+            $this->session->set_flashdata('success_register', 'Proses Pendaftaran User Berhasil');
+            redirect('welcome/login');
+        } else {
+            $this->session->set_flashdata('error', validation_errors());
+            redirect('welcome/verif');
+        }
+
+        // $this->db->select("*");
+        // $this->db->from("pasien");
+        // $this->db->where('OTP', $otp);
+        // $query = $this->db->get();
+        // return $query->result_array();
+
+        // if ($query == 1) 
+        // {
+        //     redirect('welcome/login');
+        // } else {
+        //     redirect('welcome/verif');
+        // }
+
+        // $pasien = $this->auth_pasien->getAll(); // memanggil method getAll
+        // $data['pasien'] = $pasien; // menampung di variable $data
+
+    }   
 }
--- application/models/Auth_pasien.php
+++ application/models/Auth_pasien.php
@@ -7,14 +7,38 @@ class Auth_pasien extends CI_Model
         parent::__construct();
     }
 
-    function register($username, $password)
+    public function getAll()
+    {
+        return $this->db->get('pasien');
+    }
+
+    function register($name, $username, $password, $nohp)
     {
         $data_user = array(
             'USERNAME' => $username,
             'PASSWORD' => password_hash($password, PASSWORD_DEFAULT),
             'JENIS_USER' => 'PASIEN'
         );
+        $otp = strval(rand(1000, 10000));
+        $data_user2 = array(
+            'OTP' => $otp,
+            'EMAIL_PASIEN' => $username,
+            'NAMA_PASIEN' => $name,
+            'HP_PASIEN' => $nohp
+        );
+
         $this->db->insert('user', $data_user);
+        $this->db->insert('pasien', $data_user2);
+
+    }
+
+    function verif($otp)
+    {
+        $data_user = array(
+            'STATUS_AKUN' => 1
+        );
+        $this->db->where('OTP',$otp);
+        $this->db->update('pasien',$data_user);
+        
     }
 }
-?>
