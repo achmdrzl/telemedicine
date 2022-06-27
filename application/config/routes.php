3bc8914ea0682b468740b5ee0e370230a0ae09d4
achmadbias24 <achmadbias24@gmail.com>
2022-06-27 10:25:45 +0700

    update baruuu

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'welcome';
+$route['admin'] = 'home';
 $route['dokter'] = 'dokter';
 $route['pasien'] = 'pasien';
 $route['jadwal'] = 'jadwal';
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,7 +3,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Welcome extends CI_Controller
 {
-
 	public function index()
 	{
 		render2('pasien/index');
