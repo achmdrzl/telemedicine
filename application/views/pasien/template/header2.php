e445ef30cba10e010205fb7457ec5b9c1fbfc4ec
Wildani Sulthon Mahmudi <54610995+Kaneisan@users.noreply.github.com>
2022-07-06 12:05:06 +0700

    test

--- application/views/pasien/template/header2.php
+++ application/views/pasien/template/header2.php
@@ -6,16 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <meta name="description" content="Medcity - Medical Healthcare HTML5 Template">
-    <link href="<?php echo base_url() ?>assets/pasien/images/favicon/favicon.png" rel="icon">
+    <link href="<?php echo base_url() ?>assets/pasien/images/favicon/favicon2.png" rel="icon">
     <title>Medcity - Medical Healthcare HTML5 Template</title>
+
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600;700&family=Roboto:wght@400;700&display=swap">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo base_url() ?>assets/pasien/css/libraries.css">
     <link rel="stylesheet" href="<?php echo base_url() ?>assets/pasien/css/style.css">
-    <link rel="stylesheet" href="<?php echo base_url() ?>assets/pasien/css/style2.css">
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    <script src="<?= base_url('assets/pasien/js/jquery-3.5.1.min.js') ?>"></script>
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 </head>
 
 <body>
@@ -32,16 +30,32 @@
                 <div class="container-fluid">
                     <div class="row align-items-center">
                         <div class="col-12">
-                            <div class="d-flex align-items-center justify-content-between mt-3">
+                            <div class="d-flex align-items-center justify-content-between">
                                 <ul class="contact__list d-flex flex-wrap align-items-center list-unstyled mb-0">
                                     <li>
-                                        <i class="icon-phone"></i><a href="#">Panggilan Darurat: (0321) 863502</a>
+                                        <button class="miniPopup-emergency-trigger" type="button">24/7 Emergency</button>
+                                        <div id="miniPopup-emergency" class="miniPopup miniPopup-emergency text-center">
+                                            <div class="emergency__icon">
+                                                <i class="icon-call3"></i>
+                                            </div>
+                                            <a href="tel:+201061245741" class="phone__number">
+                                                <i class="icon-phone"></i> <span> (0321) 863502</span>
+                                            </a>
+                                            <p>Please feel free to contact our friendly reception staff with any general or medical enquiry.
+                                            </p>
+                                            <a href="appointment.html" class="btn btn_secondary btnlink btn_block">
+                                                <span>Make Appointment</span> <i class="icon-arrow-right"></i>
+                                            </a>
+                                        </div><!-- /.miniPopup-emergency -->
+                                    </li>
+                                    <li>
+                                        <i class="icon-phone"></i><a href="tel:+5565454117">Panggilan Darurat: (0321) 863502</a>
                                     </li>
                                     <li>
                                         <i class="icon-location"></i><a href="#">Lokasi: Jombang, Jawa Timur</a>
                                     </li>
                                     <li>
-                                        <i class="icon-clock"></i><a href="">Senin-Jumat : 07.00-15.00 WIB</a>
+                                        <i class="icon-clock"></i><a href="contact-us.html">Mon - Fri: 8:00 am - 7:00 pm</a>
                                     </li>
                                 </ul><!-- /.contact__list -->
                                 <div class="d-flex">
@@ -72,13 +86,13 @@
                     <div class="collapse navbar-collapse" id="mainNavigation">
                         <ul class="navbar-nav ml-auto">
                             <li class="nav__item">
-                                <a href="<?php echo site_url('pasien_login/index') ?>" class="nav__item-link">Beranda</a>
+                                <a href="<?php echo site_url('welcome/index') ?>" class="nav__item-link">Beranda</a>
                             </li><!-- /.nav-item -->
                             <li class="nav__item">
                                 <a href="#" data-toggle="dropdown" class="nav__item-link">Tentang Kami</a>
                             </li><!-- /.nav-item -->
                             <li class="nav__item">
-                                <a href="<?= site_url('Dokter_pasien/index') ?>" data-toggle="dropdown" class="nav__item-link">Dokter</a>
+                                <a href="#" data-toggle="dropdown" class="nav__item-link">Dokter</a>
                             </li><!-- /.nav-item -->
                             <li class="nav__item has-dropdown">
                                 <a href="#" data-toggle="dropdown" class="dropdown-toggle nav__item-link">Layanan</a>
@@ -103,20 +117,10 @@
                         <button class="close-mobile-menu d-block d-lg-none"><i class="fas fa-times"></i></button>
                     </div><!-- /.navbar-collapse -->
                     <div class="d-none d-xl-flex align-items-center position-relative ml-30">
-                        <ul class="navbar-nav ml-auto">
-                            <li class="nav__item has-dropdown">
-                                <a href="#" data-toggle="dropdown" class="btn btn__primary btn__rounded ml-30"><i class="icon-user"></i>
-                                    <span>Profile <?php echo $this->session->ID_PASIEN; ?></span></a>
-                                <ul class="dropdown-menu">
-                                    <li class="nav__item">
-                                        <a href="<?php echo site_url('pasien_login/profile/' . $this->session->ID_PASIEN); ?>" class="nav__item-link">Informasi Akun</a>
-                                    </li><!-- /.nav-item -->
-                                    <li class="nav__item">
-                                        <a href="<?php echo site_url('pasien_login/logout') ?>" class="nav__item-link">Log Out</a>
-                                    </li><!-- /.nav-item -->
-                                </ul><!-- /.dropdown-menu -->
-                            </li><!-- /.nav-item -->
-                        </ul><!-- /.navbar-nav -->
+                        <a href="<?php echo site_url('welcome/login') ?>" class="btn btn_primary btn_rounded ml-30">
+                            <i class="icon-arrow-right"></i>
+                            <span>Login</span>
+                        </a>
                     </div>
                 </div><!-- /.container -->
             </nav><!-- /.navabr -->
